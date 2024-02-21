d82ca0ca52fd822da034619754e7ee603ee08dfa
tajamulkakroo <30586261+tajamulkakroo@users.noreply.github.com>
2024-02-21 14:45:18 +0530

    Adjusted the date format

--- Fixtures/InsuranceFixture.ts
+++ Fixtures/InsuranceFixture.ts
@@ -3,7 +3,7 @@ const faker = require('faker');
 export const HealthPartners = () => ({
     firstNameLocator: faker.random.word({ count: 8 }),
     lastNameLocator: faker.random.word({ count: 7 }),
-    birthDateLocator: faker.date.between({from: '12-01-1950', to: '12-31-2000'}).strftime("%m/%d/%Y"), // Adjust the date range as needed
+    birthDateLocator: faker.date.between({from: '12-01-1950', to: '12-31-2000'}).toLocaleDateString('en-US'), // Adjust the date range as needed
     genderLocator: faker.random.arrayElement(['Male', 'Female']),
     smokerLocator: faker.random.boolean(['Yes', 'No']),
     dependentFirstNameLocator: faker.random.word({ count: 7 }),
