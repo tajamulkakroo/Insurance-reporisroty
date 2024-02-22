0cdc50ec65a17e0fe55d8efaa8f5aa1e5b36164c
tajamulkakroo <30586261+tajamulkakroo@users.noreply.github.com>
2024-02-22 15:42:25 +0530

    Some minor fixes

--- Fixtures/InsuranceFixture.ts
+++ Fixtures/InsuranceFixture.ts
@@ -11,8 +11,8 @@ export const HealthPartners = () => ({
     childGenderLocator: faker.random.arrayElement(['Male', 'Female']),
     childSmokerLocator: faker.random.arrayElement(['Yes', 'No']),
     emailLocator: faker.internet.email(),
-    userIdLocator: faker.datatype.number({count: 8}),
-    passwordLocator: faker.internet.password({ length: 8, memorable: true }),
-    confirmPasswordLocator: faker.internet.password({ length: 8, memorable: true }),
+    userIdLocator: faker.datatype.number({count: 8}).toString(),
+    passwordLocator: faker.internet.password(10, false, /\w/, '!Aa0'),
+    confirmPasswordLocator: faker.internet.password(10, false, /\w/, '!Aa0'),
 });
 
