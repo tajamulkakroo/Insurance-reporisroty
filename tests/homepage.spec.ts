84bb20b8f5021d077e70e22040d0d18bb32e497b
tajamulkakroo <30586261+tajamulkakroo@users.noreply.github.com>
2024-03-04 22:01:11 +0530

    added some more checks

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -6,14 +6,16 @@ const fakeValue = {
   ...fakehealthPartners(),
 };
 
-test.setTimeout(1200000);
+test.setTimeout(180000);
 test('Health Partners Test', async ({ page }) => {
   const healthPartnersPage = new HealthPartnersPage(page);
     await healthPartnersPage.navigateToWelcomePage();
     await expect(page).toHaveURL("https://individualinsurance.healthpartners.com/hp/shopping/anonymous.html#welcome");
     await healthPartnersPage.clickGetStartedLink();
     await healthPartnersPage.SeeIfYouQualify();
+    await expect(page).toHaveURL("https://individualinsurance.healthpartners.com/hp/shopping/anonymous.html#view/account/WhosCoveredSE/SpecialEnrollment");
     await healthPartnersPage.fillZipCode('55413');
+    await expect(page).toHaveURL("https://individualinsurance.healthpartners.com/hp/shopping/anonymous.html#view/account/WhosCoveredSE/Demographics");
     await healthPartnersPage.fillFirstName(fakeValue.firstNameLocator);
     await healthPartnersPage.fillLastName(fakeValue.lastNameLocator);
     await healthPartnersPage.fillBirthDate('02/16/1987');
