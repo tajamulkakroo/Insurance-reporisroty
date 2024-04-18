e0d81dee2d0dc0c0fdd148017504dce1e4572497
tajamulkakroo <30586261+tajamulkakroo@users.noreply.github.com>
2024-04-18 21:18:23 +0530

    Added Spouse Page and test

---
+++ Fixtures/spouseFixture.ts
@@ -0,0 +1,26 @@
+const faker = require('faker');
+
+export const SpouseFixture = () => ({
+    lastNameLocator: faker.random.word({ count: 7 }),
+    genderLocator: faker.random.arrayElement(['Male']),
+    smokerLocator: faker.random.arrayElement(['Yes', 'No']),
+    dependentFirstNameLocator: faker.random.word({ count: 7 }),
+    dependentLastNameLocator: faker.random.word({ count: 6 }),
+    relationshipLocator: faker.random.arrayElement(['Spouse']),
+    childGenderLocator: faker.random.arrayElement(['Female']),
+    childSmokerLocator: faker.random.arrayElement(['Yes', 'No']),
+    emailLocator: faker.internet.email(),
+    userIdLocator: faker.datatype.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }).toString(),
+    passwordLocator: faker.internet.password(9, false, /[a-zA-Z0-9]/),
+    confirmPasswordLocator: faker.internet.password(9, false, /[a-zA-Z0-9]/),
+    answerLocator: faker.random.word({ count: 4, alpha: true }), 
+    addressLocator: faker.random.word({ count: 12}),
+    cityLocator: faker.random.word({ count: 10 }),
+    phoneLocator: faker.phone.phoneNumber(),
+    creditCardFirstNameLocator: faker.random.word({ count: 8 }),
+    creditCardLastNameLocator:  faker.random.word({ count: 8 }),
+    applicantFirstNameLocator: faker.random.word({ Count: 8}),
+    applicantLastNameLocator: faker.random.word({ Count: 8}),
+    confirmFirstNameLocator: faker.random.word({ Count: 8}),
+    confirmLastNameLocator: faker.random.word({ Count: 8}),
+});
---
+++ tests/policyHolderWithSpouseOnly.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { SpousePage } from '../pages/policyHolderWithSpousePage';
+import { SpouseFixture as fakehealthPartners } from '../fixtures/spouseFixture'
+
+const fakeValue = {
+  ...fakehealthPartners(),
+};
+
+test.setTimeout(180000);
+test('Health Partners Test', async ({ page }) => {
+  const spousePage = new SpousePage(page);
+    await spousePage.navigateToWelcomePage();
+    await expect(page).toHaveURL('https://individualinsurance.healthpartners.com/hp/shopping/anonymous.html#welcome');
+    await spousePage.clickGetStartedLink();
+    await spousePage.SeeIfYouQualify();
+    await spousePage.saveAndContinue();
+    await spousePage.fillZipCode('55413');
+    await expect(page).toHaveURL("https://individualinsurance.healthpartners.com/hp/shopping/anonymous.html#view/account/WhosCoveredSE/Demographics");
+    await spousePage.fillFirstName();
+    await spousePage.fillLastName(fakeValue.lastNameLocator);
+    await spousePage.fillBirthDate('02/16/1987');
+    await spousePage.selectGender(fakeValue.genderLocator);
+    await spousePage.selectSmokerOption(fakeValue.smokerLocator);
+    await spousePage.clickAddDependentLink();
+    await spousePage.fillDependentFirstName(fakeValue.dependentFirstNameLocator);
+    await spousePage.fillDependentLastName(fakeValue.dependentLastNameLocator);
+    await spousePage.fillDependentBirthDate('12/18/2001');
+    await spousePage.selectDependentRelationship(fakeValue.relationshipLocator);
+    await spousePage.selectDependentGender(fakeValue.childGenderLocator);
+    await spousePage.selectSmokerOptionForDepandant(fakeValue.childSmokerLocator);
+    await spousePage.clickContinueLink();
+    await spousePage.selectBrowseAllPlans();
+    await spousePage.addToCart();
+    await spousePage.viewCartAndEnroll();
+    await spousePage.enrollNow();
+    await spousePage.fillEmailAddress(fakeValue.emailLocator);
+    await spousePage.userIDAndPassword(fakeValue.userIdLocator, fakeValue.passwordLocator);
+    await spousePage.securityQuestionAndBroker(fakeValue.answerLocator);
+    await spousePage.startEnrolment(fakeValue.addressLocator, fakeValue.cityLocator, fakeValue.phoneLocator);
+    await spousePage.familyMembers();
+    await spousePage.planInformation();
+    await spousePage.uploadFile();
+    await spousePage.paymentOptions(fakeValue.lastNameLocator);
+    await spousePage.verifySubmission();
+    await spousePage.submitForm(fakeValue.lastNameLocator);
+
+});
