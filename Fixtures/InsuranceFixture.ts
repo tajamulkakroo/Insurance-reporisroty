728a4b10c46b365e0eb493ec04a150c70a8d71e8
tajamulkakroo <30586261+tajamulkakroo@users.noreply.github.com>
2024-04-22 21:21:47 +0530

    Minor fixes with the faker file

--- Fixtures/InsuranceFixture.ts
+++ Fixtures/InsuranceFixture.ts
@@ -1,7 +1,7 @@
 const faker = require('faker');
 
 export const HealthPartners = () => ({
-    lastNameLocator: faker.random.word({ count: 7 }),
+    lastNameLocator: faker.random.word({ count: 7 }).replace(/[^a-zA-Z]/g, ''),
     genderLocator: faker.random.arrayElement(['Male', 'Female']),
     smokerLocator: faker.random.arrayElement(['Yes', 'No']),
     dependentFirstNameLocator: faker.random.word({ count: 7 }),
@@ -11,16 +11,16 @@ export const HealthPartners = () => ({
     childSmokerLocator: faker.random.arrayElement(['Yes', 'No']),
     emailLocator: faker.internet.email(),
     userIdLocator: faker.datatype.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }).toString(),
-    passwordLocator: faker.internet.password(9, false, /[a-zA-Z0-9]/),
-    confirmPasswordLocator: faker.internet.password(9, false, /[a-zA-Z0-9]/),
+    passwordLocator: faker.internet.password(9, false, /^[a-zA-Z0-9]+$/), // Excludes special characters
+    confirmPasswordLocator: faker.internet.password(9, false, /^[a-zA-Z0-9]+$/), // Excludes special characters
     answerLocator: faker.random.word({ count: 4, alpha: true }), 
-    addressLocator: faker.random.word({ count: 12}),
-    cityLocator: faker.random.word({ count: 10 }),
+    addressLocator: faker.random.word({ count: 12 }),
+    cityLocator: faker.random.word({ count: 10 }).replace(/[^a-zA-Z]/g, ''), // Excludes special characters
     phoneLocator: faker.phone.phoneNumber(),
     creditCardFirstNameLocator: faker.random.word({ count: 8 }),
     creditCardLastNameLocator:  faker.random.word({ count: 8 }),
-    applicantFirstNameLocator: faker.random.word({ Count: 8}),
-    applicantLastNameLocator: faker.random.word({ Count: 8}),
-    confirmFirstNameLocator: faker.random.word({ Count: 8}),
-    confirmLastNameLocator: faker.random.word({ Count: 8}),
+    applicantFirstNameLocator: faker.random.word({ Count: 8 }),
+    applicantLastNameLocator: faker.random.word({ Count: 8 }),
+    confirmFirstNameLocator: faker.random.word({ Count: 8 }),
+    confirmLastNameLocator: faker.random.word({ Count: 8 }),
 });
